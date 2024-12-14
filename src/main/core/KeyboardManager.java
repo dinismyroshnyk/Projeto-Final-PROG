c70dab65cc598fb11d0e3393fdc47b0d893f0e69
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2024-12-14 11:35:49 +0000

    feat: Refactor `KeyboardManager`

--- src/main/core/KeyboardManager.java
+++ src/main/core/KeyboardManager.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class KeyboardManager {
     private static KeyboardManager keyboardManager;
 
@@ -16,6 +15,50 @@ public class KeyboardManager {
         ENTER,
         BACKSPACE,
         Q
+    }
+
+    public enum EscapeSequence {
+        UP(new int[]{91, 65}, new int[] {79, 65}),
+        DOWN(new int[]{91, 66}, new int[] {79, 66}),
+        RIGHT(new int[]{91, 67}, new int[] {79, 67}),
+        LEFT(new int[]{91, 68}, new int[] {79, 68});
+
+        private int[] seq1;
+        private int[] seq2;
+
+        EscapeSequence(int[] seq1, int[] seq2) {
+            this.seq1 = seq1;
+            this.seq2 = seq2;
+        }
+
+        public int[] getSeq1() { return seq1; }
+        public int[] getSeq2() { return seq2; }
+
+        public static Keys getKeyBySequence(int[] seq){
+            for (EscapeSequence escapeSequence : values()){
+                if(arrayEquals(escapeSequence.getSeq1(), seq) || arrayEquals(escapeSequence.getSeq2(), seq)){
+                    switch (escapeSequence){
+                        case UP: return Keys.ARROW_UP;
+                        case DOWN: return Keys.ARROW_DOWN;
+                        case LEFT: return Keys.ARROW_LEFT;
+                        case RIGHT: return Keys.ARROW_RIGHT;
+                    }
+                }
+            }
+            return null;
+        }
+
+        private static boolean arrayEquals(int[] arr1, int[] arr2) {
+            if (arr1.length != arr2.length) {
+                return false;
+            }
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr1[i] != arr2[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 
     private Map<Keys, Boolean> keyStates = new HashMap<>();
@@ -35,30 +78,39 @@ public class KeyboardManager {
 
     public void update(int key) {
         switch (key) {
-            case 27: // Escape sequence start
-                // Handle escape sequences (arrow keys, etc.)
-                try {
-                    int nextKey1 = InterfaceManager.getInstance().getTerminal().reader().read();
-                    int nextKey2 = InterfaceManager.getInstance().getTerminal().reader().read();
-                    if(nextKey1 == 91 || nextKey1 == 79){ //ESC [
-                        switch(nextKey2){
-                            case 65: keyStates.put(Keys.ARROW_UP, true); break; //Up
-                            case 66: keyStates.put(Keys.ARROW_DOWN, true); break; //Down
-                            case 67: keyStates.put(Keys.ARROW_RIGHT, true); break; //Right
-                            case 68: keyStates.put(Keys.ARROW_LEFT, true); break; //Left
-                        }
-                    }
-                } catch (IOException e){
-                    e.printStackTrace();
-                }
+            case 27:
+                handleEscapeSequence();
                 break;
-            case 10: case 13: keyStates.put(Keys.ENTER, true); break; //Enter
-            case 8: case 127: keyStates.put(Keys.BACKSPACE, true); break; //Backspace
-            case 'q': keyStates.put(Keys.Q, true); break; //q
+            case 10: case 13:
+                handleEnter();
+                break;
+            case 8: case 127:
+                handleBackspace();
+                break;
+            case 'q':
+                handleQ();
+                break;
             default:
                 // Ignore other keys
         }
     }
+
+    private void handleEscapeSequence() {
+        try {
+            int nextKey1 = InterfaceManager.getInstance().getTerminal().reader().read();
+            int nextKey2 = InterfaceManager.getInstance().getTerminal().reader().read();
+            Keys key = EscapeSequence.getKeyBySequence(new int[]{nextKey1, nextKey2});
+            if(key != null){
+                keyStates.put(key, true);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleEnter() { keyStates.put(Keys.ENTER, true); }
+    private void handleBackspace() { keyStates.put(Keys.BACKSPACE, true); }
+    private void handleQ() { keyStates.put(Keys.Q, true); }
 
     public void resetKeyStates() {
         for (Keys key : Keys.values()) {
@@ -66,8 +118,5 @@ public class KeyboardManager {
         }
     }
 
-
-    public boolean isKeyPressed(Keys key) {
-        return keyStates.get(key);
-    }
+    public boolean isKeyPressed(Keys key) { return keyStates.get(key); }
 }
