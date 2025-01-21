6fccf818b212cf7c5c19e90ad02cc43bbeaec21c
Fiugas <95915531+Fiugas@users.noreply.github.com>
2025-01-21 23:01:13 +0000

    Registro de novo utilizidador

--- src/main/data/models/User.java
+++ src/main/data/models/User.java
@@ -19,4 +19,47 @@ public abstract class User {
         TECHNICIAN,
         CLIENT
     }
+
+    public User(String login, String password, String name, String email, UserType type) {
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+        this.type = type;
+        this.status = UserStatus.ACTIVE;
+    }
+
+    // Getters
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    // Setters
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //public void setStatus(UserStatus status) {
+    //   this.status = status;
+    //}
 }
